1ee37190f1f48aa7ba67c7387f6b3c80303a1303
fither <onurum.3411@hotmail.com>
2020-05-28 22:45:10 +0300

    test

---
+++ index.js
@@ -0,0 +1,27 @@
+const app = require('express')();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get('/', (req, res) => {
+	res.json({
+		firstname: 'BESTCLOUDFORME',
+		lastname: 'BESTCLOUDFORME'
+	})
+})
+
+app.get('/whoami', (req, res) => {
+	res.json({
+		firstname: (req.query.firstname) ? req.query.firstname : null,
+		lastname: (req.query.lastname) ? req.query.lastname : null,
+	})
+})
+
+app.post('/whoami', (req, res) => {
+	res.json({
+		firstname: (req.body.firstname) ? req.body.firstname : null,
+		lastname: (req.body.lastname) ? req.body.lastname : null,
+	})
+})
+
+app.listen(8080)
